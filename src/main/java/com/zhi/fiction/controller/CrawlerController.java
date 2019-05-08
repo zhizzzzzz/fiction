d4cd8ca5facbefe2305a0af9fb46777027e30cbd
yongzhi.zhao <yongzhi.zhao@china.zhaogang.com>
2019-05-08 09:23:55 +0800

    接入redisson

--- src/main/java/com/zhi/fiction/controller/CrawlerController.java
+++ src/main/java/com/zhi/fiction/controller/CrawlerController.java
@@ -1,5 +1,10 @@
 package com.zhi.fiction.controller;
 
+import java.io.IOException;
+import java.util.HashMap;
+
+import org.redisson.api.RBuckets;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +12,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.zhi.fiction.biz.CrawlerBiz;
+import com.zhi.fiction.util.RedissonUtil;
 
 /** 
 * @author 作者 yongzhi.zhao: 
@@ -19,12 +25,32 @@ import com.zhi.fiction.biz.CrawlerBiz;
 public class CrawlerController {
 
     @Autowired
-    private CrawlerBiz crawlerBiz;
+    private CrawlerBiz   crawlerBiz;
+
+    @Autowired
+    private RedissonUtil redissonUtil;
 
     @ResponseBody
     @RequestMapping(value = "/findSource")
     public void findSource() {
         crawlerBiz.resolveHtmlStr();
+    }
+
+    @RequestMapping(value = "/test")
+    public void test() {
+
+        try {
+            RedissonClient redisson = redissonUtil.redissonClient();
+            HashMap m = new HashMap();
+            m.put("zhitest", "1312");
+            RBuckets rb = redisson.getBuckets();
+            rb.set(m);
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
     }
 
 }
---
+++ src/main/java/com/zhi/fiction/util/RedissonUtil.java
@@ -0,0 +1,26 @@
+package com.zhi.fiction.util;
+
+import java.io.IOException;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+@Configuration
+public class RedissonUtil {
+
+    /**
+     * redisson客户端
+     * @return
+     * @throws IOException
+     */
+    @Bean(destroyMethod = "shutdown")
+    public RedissonClient redissonClient() throws IOException {
+        Config config = Config.fromYAML(new ClassPathResource("redisson.yml").getInputStream());
+        return Redisson.create(config);
+    }
+
+}
